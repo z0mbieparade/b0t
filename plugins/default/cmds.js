27298760de17553178bfb4b53be26d4e9f2f63fb
Nutmeg Anne <z0p@chain5warm.local>
2017-02-18 21:55:21 -0700

    added no highlight to list

--- plugins/default/cmds.js
+++ plugins/default/cmds.js
@@ -287,6 +287,7 @@ var cmds = {
         perm: 'owner',
         func: function(action, nick, chan, args, command_string){ 
             action.get_all_users_in_chan_data(null, function(data){
+                data = data.map(action.no_highlight);
                 action.say(data, 1, {skip_verify: true, join: ', ', skip_buffer: true});
             });
         }
